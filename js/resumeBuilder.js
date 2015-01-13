d078964d80a784dddcbbdf92cf9396928103efe6
opqr549 <drummerdude2112@gmail.com>
2015-01-13 00:46:43 -0700

    Add name and role to header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,8 @@
 $("#main").append("Chris Meese");
+
+var formattedName = HTMLheaderName.replace("%data%", "Chris Meese");
+var role = "Web Devoo";
+var formattedRole = HTMLheaderRole.replace("%data%", "Devooo");
+
+$("#header").prepend(formattedName);
+$("#header").prepend(formattedRole);
